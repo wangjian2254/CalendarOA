707fc125931c16d43318a78ce14b17c7392857bb
王健 <wangjian2254@gmail.com>
2014-04-02 09:21:54 +0800

    分组管理开发

--- rili/view_group.py
+++ rili/view_group.py
@@ -3,39 +3,53 @@
 #Date: 14-3-28
 #Time: 上午6:43
 from django.contrib.auth.models import User
+from django.db import transaction
 from django.db.models import Q
-from rili.models import Group
+from rili.models import Group, RiLiWarning, Schedule
 from util.jsonresult import getResult
 from util.loginrequired import client_login_required
 
 __author__ = u'王健'
 
+def groupTodict(group):
+    return {'id': group.pk, 'name': group.name, 'author': group.author.username, 'color': group.color,
+                  'userlist': [{'username': u.username, 'nickname': u.first_name} for u in group.users.all()], 'observerlist':[{'username': u.username, 'nickname': u.first_name} for u in group.observers.all()]}
 
 
 @client_login_required
 def getMyGroup(request):
-    user = request.user
-
-    groupquery = Group.objects.filter(Q(author=user) | Q(users=user)).order_by('id')
+    u = request.user
     l = []
-    for group in groupquery:
-        l.append({'id': group.pk, 'name': group.name, 'author': group.author_id, 'color': group.color,
-                  'userlist': [{'username': u.username, 'nickname': u.first_name} for u in group.users.all()]})
+    for group in u.group_sharedobservers.all():
+        g = groupTodict(group)
+        g['pem'] ='look'
+        l.append(g)
+    for group in u.group_sharedusers.all():
+        g = groupTodict(group)
+        g['pem'] ='join'
+        l.append(g)
+    for group in u.group_set.all():
+        g = groupTodict(group)
+        g['pem'] ='create'
+        l.append(g)
+
     if len(l) == 0:
         g = Group()
-        g.author = user
+        g.author = u
         g.color = 0xeaeaea
         g.flag = 'default'
-        g.name = u'%s的日程' % user.first_name
+        g.name = u'%s的日程' % u.first_name
         g.save()
     return getResult(True, '', l)
 
 @client_login_required
+@transaction.commit_on_success
 def updateGroup(request):
     id = request.REQUEST.get('id','')
     name = request.REQUEST.get('name','')
     color = request.REQUEST.get('color','')
     join = request.REQUEST.getlist('joins')
+    observers = request.REQUEST.getlist('observers')
 
     if id:
         group = Group.objects.get(pk=id)
@@ -49,29 +63,59 @@ def updateGroup(request):
 
     if join:
         group.users = User.objects.filter(username__in=join)
+        group.observers = User.objects.filter(username__in=observers)
         group.save()
 
-    return getResult(True,'创建成功',)
+    return getResult(True,'保存成功',group.pk)
 
 
 
 @client_login_required
+@transaction.commit_on_success
 def joinGroup(request):
     id = request.REQUEST.get('id','')
     username = request.REQUEST.get('username','')
     do = request.REQUEST.get('do','')
+    usertype = request.REQUEST.get('type','join')
     if id:
         group = Group.objects.get(pk=id)
         if do =='join':
-            if username!=group.author.username and 0==group.users.filter(username=username).count():
+            if usertype=='join' and  username!=group.author.username and 0==group.users.filter(username=username).count():
                 group.users.add(User.objects.get(username=username))
-                group.save()
-        elif do == 'out':
-            if username!=group.author.username and 1==group.users.filter(username=username).count():
+                group.observers.remove(User.objects.get(username=username))
+                group.users.save()
+                group.observers.save()
+            if usertype=='observer' and  username!=group.author.username and 0==group.observers.filter(username=username).count():
+                group.observers.add(User.objects.get(username=username))
                 group.users.remove(User.objects.get(username=username))
-                group.save()
+                group.users.save()
+                group.observers.save()
+        elif do == 'out':
+            if username!=group.author.username and (1==group.users.filter(username=username).count() or 1==group.observers.filter(username=username).count()):
+                group.users.remove(User.objects.get(username=username))
+                group.observers.remove(User.objects.get(username=username))
+                group.users.save()
+                group.observers.save()
         else:
             return getResult(False,u'操作不正确',id)
+        return getResult(True,u'完成操作',id)
+    else:
+        return getResult(False,u'操作不正确',id)
+
+@client_login_required
+@transaction.commit_on_success
+def delGroup(request):
+    id = request.REQUEST.get('id','')
+
+    if id:
+        group = Group.objects.get(pk=id)
+
+        if group.author.username==request.user.username:
+            RiLiWarning.objects.filter(type='Schedule',fatherid__in=Schedule.objects.filter(group=group)).delete()
+            Schedule.objects.filter(group=group).delete()
+            group.delete()
+        else:
+            return getResult(False,u'不具有删除权限',id)
         return getResult(True,u'完成操作',id)
     else:
         return getResult(False,u'操作不正确',id)
