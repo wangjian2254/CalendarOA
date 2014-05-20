90bc17b394cc8640e97eacb265bfb15442546d3b
王健 <wangjian2254@gmail.com>
2014-05-20 08:04:32 +0800

    考试模块 url设计

--- kaoshi/urls.py
+++ kaoshi/urls.py
@@ -5,19 +5,36 @@ Created on 2011-3-19
 @author: 王健
 '''
 from django.conf.urls import patterns
-from kaoshi.views import  savesubjectoption, showsubjectoption, showTouPiao, toupiaoPage, toupiaoResult, endnewslist,toupiaoerror, toupiaoExcel
 
 
 urlpatterns = patterns('kaoshi',
 
-                       (r'^savesubjectoption/$', savesubjectoption),
-                       (r'^showsubjectoption/$', showsubjectoption),
-                       (r'^showTouPiao/$', showTouPiao),
-                       (r'^toupiaoPage/$', toupiaoPage),
-                       (r'^toupiaoResult/$', toupiaoResult),
-                       (r'^endnewslist/$', endnewslist),
-                       (r'^error/$', toupiaoerror),
-                       (r'^toupiaoExcel/$', toupiaoExcel),
+                       (r'^updateKind/$', 'view_kinds.updateKind'),
+                       (r'^kindList/$', 'view_kinds.getAllKind'),
+                       (r'^kindDel/$', 'view_kinds.delKind'),
+
+                       (r'^updatePaperKind/$', 'view_paperkinds.updatePaperKind'),
+                       (r'^paperKindList/$', 'view_paperkinds.getAllPaperKind'),
+                       (r'^paperKindDel/$', 'view_paperkinds.delPaperKind'),
+
+                       (r'^updateSubject/$', 'view_kinds.updateKind'),
+                       (r'^subjectList/$', 'view_kinds.updateKind'),
+                       (r'^subjectDel/$', 'view_kinds.updateKind'),
+                       (r'^subjectDoOption/$', 'view_kinds.updateKind'),
+
+                       (r'^updateOption/$', 'view_kinds.updateKind'),
+                       (r'^optionList/$', 'view_kinds.updateKind'),
+                       (r'^optionDel/$', 'view_kinds.updateKind'),
+
+                       (r'^updatePaper/$', 'view_kinds.updateKind'),
+                       (r'^getPaper/$', 'view_kinds.updateKind'),
+                       (r'^answerPaper/$', 'view_kinds.updateKind'),
+                       (r'^paperDoSubject/$', 'view_kinds.updateKind'),
+                       (r'^paperList/$', 'view_kinds.updateKind'),
+                       (r'^paperDel/$', 'view_kinds.updateKind'),
+                       (r'^paperCopy/$', 'view_kinds.updateKind'),
+
+                       (r'^toupiaoExcel/$', 'view_kinds.updateKind'),
 
 
 
