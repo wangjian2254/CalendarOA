aeb661eda1bfeb1f6d3d13a786405273540d9bfb
王健 <wangjian2254@gmail.com>
2014-07-11 09:08:02 +0800

    配置 部署

--- CalendarOA/settings.py
+++ CalendarOA/settings.py
@@ -23,11 +23,11 @@ MANAGERS = ADMINS
 # }
 DATABASES = {
       'default': {
-          'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+          'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
           'NAME': 'calendar',                      # Or path to database file if using sqlite3.
           'USER': 'root',                      # Not used with sqlite3.
-          'PASSWORD': 'htfsdb',                  # Not used with sqlite3.
-          'HOST': '192.168.101.4',                      # Set to empty string for localhost. Not used with sqlite3.
+          'PASSWORD': '123456',                  # Not used with sqlite3.
+          'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
           'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
           'ATOMIC_REQUEST': True,
       }
